b115aaae11616cccbcb6fdc166ac2f7cbabdb171
viktor <vkibank@gmail.com>
2020-03-14 21:15:23 +0200

    Listen push

--- service-worker.js
+++ service-worker.js
@@ -16,3 +16,31 @@ self.addEventListener('notificationclick', function (e) {
 self.addEventListener('notificationclose', function (e) {
     console.log('Notification was closed', e);
 });
+
+self.addEventListener('push', function (e) {
+   console.log('Push Notification Recieved', e);
+   var data = {title: 'Default Title', content: 'Default Content!!!'};
+   if(e.data) {
+        data = JSON.parse(e.data.text());
+   }
+
+   var options = {
+       body: data.content,
+       icon: '/app-icon-96x96.png',
+       image: '/sf-boat.jpg',
+       dir: 'ltr',
+       lang: 'en-US',
+       vibrate: [100, 50, 200],
+       badge: '/app-icon-96x96.png',
+       tag: 'confirm-notification',
+       renotify: true,
+       actions: [
+           {action: 'confirm', title: 'Okay', icon: '/app-icon-96x96.png'},
+           {action: 'cancel', title: 'Cancel', icon: '/app-icon-96x96.png'}
+       ]
+   };
+
+   e.waitUntil(
+       self.registration.showNotification(data.title, options)
+   );
+});
